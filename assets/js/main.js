6a54ca2b74534ae6f6dec69bc730f3032af915a3
Siddhartha Khuntia <77291662+sidkhuntia@users.noreply.github.com>
2021-05-30 16:04:11 +0530

    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,33 +77,12 @@
     onscroll(document, headerScrolled)
   }
 
-
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
 
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
@@ -146,22 +125,6 @@
 
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
-  });
-
-
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
   });
 
 
